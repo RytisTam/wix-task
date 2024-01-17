650016bba91dc96b58a2caf6d921aa237ecd9be6
Rytis Tamošiūnas <rytis@tamosiunas.dev>
2024-01-17 14:05:31 +0200

    finished task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,25 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import CsvInput from '../../components/CsvInput'
+import { useEffect, useState } from 'react'
+import RenderCsv from '../../components/RenderCsv'
 
 export default function Home() {
+  const [csvData, setCsvData] = useState<string>(``)
+  const [firstRowHeader, setFirstRowHeader] = useState<boolean>(false)
+
+  useEffect(() => {setCsvData(`John,Doe,120 jefferson st.,Riverside, NJ, 08075
+  Jack,McGinnis,220 hobo Av.,Phila, PA,09119
+  "John ""Da Man""",Repici,120 Jefferson St.,Riverside, NJ,08075
+  Stephen,Tyler,"7452 Terrace ""At the Plaza"" road",SomeTown,SD, 91234
+  ,Blankman,,SomeTown, SD, 00298
+  "Joan ""the bone"", Anne",Jet,"9th, at Terrace plc",Desert City,CO,00123`)},[])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  <>
+    <CsvInput csvData={csvData} setCsvData={setCsvData} setFirstRowHeader={setFirstRowHeader}/>
+    {csvData && <RenderCsv csvData={csvData} firstRowHeader={firstRowHeader}/>}
+  </>
   )
 }
